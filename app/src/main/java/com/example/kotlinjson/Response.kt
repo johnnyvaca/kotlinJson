f111c9f2890bcba7d7209113bff4210a39121c4d
VACA-JARAMILLO Johnny <Johnny.VACA-JARAMILLO@cpnv.ch>
2020-12-17 16:54:34 +0100

    add

--- app/src/main/java/com/example/kotlinjson/Response.kt
+++ app/src/main/java/com/example/kotlinjson/Response.kt
@@ -1,4 +1,18 @@
 package com.example.kotlinjson
-
+// https://geekytheory.com/trabajando-con-json-en-android
 class Response {
-}
+
+    var json = CADENA_JSON;
+    val lista_frutas: List<User> = ArrayList(); //inicializamos la lista donde almacenaremos los objetos Fruta
+
+    JSONObject
+    object = new JSONObject(json); //Creamos un objeto JSON a partir de la cadena
+    JSONArray json_array =
+    object.optJSONArray("frutas"); //cogemos cada uno de los elementos dentro de la etiqueta "frutas"
+
+    for (int i = 0; i < json_array.length(); i++)
+    {
+        lista_frutas.add(new Fruta (json_array.getJSONObject(i)));
+
+
+    }
